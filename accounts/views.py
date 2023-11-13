bb72ea8c2c7cfa2848419b262258b21c6f09320e
nino <nino_roldan@yahoo.com>
2023-11-13 11:05:13 +0900

    started the templates setting

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('products')
+    return render(request, 'accounts/customer.html')
 
 def services(request):
     return HttpResponse('services')
