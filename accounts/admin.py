7a21521201a5cd6338f5f203d46571371e2203ab
nino <nino_roldan@yahoo.com>
2023-12-01 00:32:42 +0900

    registered the Customers database in the admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#lines below are needed to register the Customer database in the Admin panel
+from .models import Customer
+admin.site.register(Customer) 
