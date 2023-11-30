13644463c3bde8c8d2ed24a0a4d48acab08355ec
nino <nino_roldan@yahoo.com>
2023-12-01 00:29:23 +0900

    created migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50, null=True) #null=True essentially makes it OK to not set a default value
+    phone = models.CharField(max_length=30, null=True)
+    email = models.CharField(max_length=50, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
