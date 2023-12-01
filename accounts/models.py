848226a331b397402fdb0b83c4f39d5eb20dee41
nino <nino_roldan@yahoo.com>
2023-12-01 17:01:34 +0900

    built Order and Product models

--- accounts/models.py
+++ accounts/models.py
@@ -8,4 +8,32 @@ class Customer(models.Model):
     email = models.CharField(max_length=50, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name + "  " + self.phone
+
+class Product(models.Model):
+    CATEGORY = (
+                    ('Indoor'),
+                    ('Outdoor'),
+
+    )
+
+    name = models.CharField(max_length=50, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+class Order(models.Model):
+    STATUS = (
+                ('Pending', 'Pending'),
+                ('Out for delivery', 'Out for delivery'),
+                ('Delivered', 'Delivered')
+    )
+    
+    
+    #customer = 
+    #product  =
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, null=True, choices=STATUS)
 
