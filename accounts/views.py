aba6c344bb26c4c23f604f6dcde9462cad6b7c9c
nino <nino_roldan@yahoo.com>
2023-12-26 13:05:43 +0900

    populating the Product portion with names)

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all() #this will gather all of the Products from the model
+    return render(request, 'accounts/products.html', {'products' : products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
