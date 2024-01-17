4c972449dd7e6cf669f4ea88f254edb0fc78f924
nino <nino_roldan@yahoo.com>
2024-01-17 17:11:04 +0900

    finishing up on the delete mechanism

--- accounts/views.py
+++ accounts/views.py
@@ -72,8 +72,12 @@ def updateOrder(request, pk):
 
 
 def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
     context = {
-        
+        'item' : order
     }
     return render(request, 'accounts/delete.html', context)
 
